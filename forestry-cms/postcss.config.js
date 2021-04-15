5588493d557bc520fd59d78c1261fafe8b9d6f5f
Richard Brus <richard@seoworx.net.au>
2021-04-15 22:39:04 +1000

    first commit

--- forestry-cms/postcss.config.js
+++ forestry-cms/postcss.config.js
@@ -4,11 +4,16 @@ module.exports = {
             overrideBrowserslist: ["> 0.5% in US", "Safari > 9"]
         }),
         require('postcss-combine-media-query'),
+
+        // ------------------------------------------
+        //  The New plug-in would go here
+        // ------------------------------------------
+
         // require('postcss-merge-rules'),
-        require('postcss-merge-rules2'),
+        // require('postcss-merge-rules2'),
         // require('postcss-combine-duplicated-selectors')({
         //     removeDuplicatedValues: true
         // }),
-        require('postcss-discard-duplicates')
+        // require('postcss-discard-duplicates')
     ]
 };
